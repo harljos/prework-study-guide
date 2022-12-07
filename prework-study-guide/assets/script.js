7abd10f8d175721e33b57f79f13b8d6ad37fbb17
unknown <joseph.harley02@gmail.com>
2022-12-07 11:21:21 -0500

    Added JavaScript file and added javascript to the file

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,22 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var i = 0; i < topics.length; i++){
+    console.log(topics[i]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("Lets's study HTML!");
+}
+else if (topic === 'CSS') {
+    console.log("Lets's study CSS!");
+}
+else if (topic === 'Git') {
+    console.log("Let's stydy Git");
+}
+else if (topic === 'Javascript') {
+    console.log("Let's study JavaScript!");
+}
+else {
+    console.log('Please try again');
+}
+*/
