9bb95b1a693d50c557267d1d4e67b7811d8fd27b
unknown <joseph.harley02@gmail.com>
2022-12-07 14:09:33 -0500

    added javascript code to make the site more interactive and added notes to the javascript section of the HTML file

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,22 +1,31 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var i = 0; i < topics.length; i++){
-    console.log(topics[i]);
+function listTopics() {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
-/*
-if (topic === 'HTML') {
-    console.log("Lets's study HTML!");
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Lets's study HTML!");
+    }  
+    else if (randomTopic === 'CSS') {
+        console.log("Lets's study CSS!");
+    }
+    else if (randomTopic === 'Git') {
+        console.log("Let's stydy Git");
+    }
+    else if (randomTopic === 'Javascript') {
+        console.log("Let's study JavaScript!");
+    }
+    else {
+        console.log('Please try again');
+    }
 }
-else if (topic === 'CSS') {
-    console.log("Lets's study CSS!");
-}
-else if (topic === 'Git') {
-    console.log("Let's stydy Git");
-}
-else if (topic === 'Javascript') {
-    console.log("Let's study JavaScript!");
-}
-else {
-    console.log('Please try again');
-}
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topics should we study first?');
+selectTopic();
